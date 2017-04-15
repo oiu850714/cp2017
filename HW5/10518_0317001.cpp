60eee563d9c3a7ea970680b20bc5bb14610e58bb
oiu850714 <oiu850714@gmail.com>
2017-04-15 22:13:21 +0800

    comment the fucking shit I coded.

--- HW5/10518_0317001.cpp
+++ HW5/10518_0317001.cpp
@@ -35,18 +35,20 @@ int main()
         while(permutation_pool.size() > 1)
         {
             unsigned long long temp_fac = factorial(permutation_pool.size() - 1);
+            unsigned long long n_element = 
+            /*
             for(unsigned long long index = 0; index < permutation_pool.size(); index++)
             {
                 if(cur_ith + (index * temp_fac) >= i)
                 {
-                    /*
+                    
                     printf("===============\n");
                     printf("cur_ith: %llu\n", cur_ith);
                     printf("cur_index: %llu\n", index);
                     printf("index * temp_fac :%llu\n", index * temp_fac);
                     printf("cur_ith + (index * temp_fac): %llu\n", cur_ith + (index*temp_fac));
                     printf("i: %llu\n", i);
-                    */
+                    
                     cur_ith += (index - 1)* temp_fac;
                     ans.push_back(permutation_pool[index - 1]);
                     permutation_pool.erase(permutation_pool.begin() + (index - 1));
@@ -54,20 +56,21 @@ int main()
                 }
                 if(index == permutation_pool.size() - 1)
                 {
-                    /*
+                    
                     printf("===============\n");
                     printf("cur_ith: %llu\n", cur_ith);
                     printf("cur_index: %llu\n", index);
                     printf("index * temp_fac :%llu\n", index * temp_fac);
                     printf("cur_ith + (index * temp_fac): %llu\n", cur_ith + (index*temp_fac));
                     printf("i: %llu\n", i);
-                    */
+                    
                     cur_ith += index * temp_fac;
                     ans.push_back(permutation_pool[index]);
                     permutation_pool.pop_back();
                     break;
                 }
-            }
+            }*/
+
         }
         ans.push_back(permutation_pool[0]);
         permutation_pool.pop_back();
